52ff5243880f9561ab41513cf62fde2d7752b1ff
lason69 <jacek.lason@gmail.com>
2016-12-04 15:35:22 +0100

    Add files via upload

--- app/movieDetail/movieDetailController.js
+++ app/movieDetail/movieDetailController.js
@@ -3,54 +3,54 @@
 
     angular
         .module('app')
-          .controller('movieDetailController', ['$scope', '$http', '$q', '$routeParams', 'movieDetailService', movieDetailController])
+          .controller('movieDetailController', [ '$http', '$q', '$routeParams', 'movieDetailService', movieDetailController])
 
 
 
-    function movieDetailController($scope, $http, $q, $routeParams, movieDetailService) {
-    
+    function movieDetailController($http, $q, $routeParams, movieDetailService) {
+        var vm = this;
         this.params = $routeParams;
         console.log(this.params.movieID)
         
-        $scope.movieID = this.params.movieID;
+        vm.movieID = this.params.movieID;
         fillMovie();
         fillCast();
-        $scope.collapsed = false;
+        vm.collapsed = false;
 
         function fillMovie() {
            getMovie().then(function (model) {
 
-                $scope.Movie = model;
+               vm.Movie = model;
 
             })
         }
         function fillCast() {
             getAllCast().then(function (model) {
 
-                $scope.Cast = model.cast;
-                $scope.Crew = model.crew;
+                vm.Cast = model.cast;
+                vm.Crew = model.crew;
                 console.log(model);
             })
         }
-        $scope.getCast = function () {
-            return $scope.Cast.slice(0, 6);
+        vm.getCast = function () {
+            return vm.Cast.slice(0, 6);
         }
-        $scope.getRestCast = function () {
-            console.log($scope.collapsed);
-            if ($scope.collapsed) {
-                $scope.collapsed = false;
+        vm.getRestCast = function () {
+            console.log(vm.collapsed);
+            if (vm.collapsed) {
+                vm.collapsed = false;
                 return;
             }
-            $scope.RestCast = $scope.Cast.slice(6, $scope.Cast.length - 1)
-            $scope.collapsed = true;
-            return $scope.RestCast;
+            vm.RestCast = vm.Cast.slice(6, vm.Cast.length - 1)
+            vm.collapsed = true;
+            return vm.RestCast;
         }
 
         function getMovie() {
 
  
 
-            return $http.get('https://api.themoviedb.org/3/movie/' + $scope.movieID + '?api_key=bc246856648f34ccdf9aef4b69a26470&language=en-US')
+            return $http.get('https://api.themoviedb.org/3/movie/' + vm.movieID + '?api_key=bc246856648f34ccdf9aef4b69a26470&language=en-US')
             .then(function (data) {
                 return data.data
                
@@ -63,7 +63,7 @@
         }
 
         function getAllCast() {
-            return $http.get('https://api.themoviedb.org/3/movie/' + $scope.movieID + '/credits?api_key=bc246856648f34ccdf9aef4b69a26470&language=en-US')
+            return $http.get('https://api.themoviedb.org/3/movie/' + vm.movieID + '/credits?api_key=bc246856648f34ccdf9aef4b69a26470&language=en-US')
             .then(function (data) {
                 return data.data;
 
@@ -75,7 +75,7 @@
         }
 
         function collapseIcon() {
-            return ($scope.collapsed) ? 'glyphicon glyphicon-chevron-down' : 'glyphicon glyphicon-chevron-up';
+            return (vm.collapsed) ? 'glyphicon glyphicon-chevron-down' : 'glyphicon glyphicon-chevron-up';
         }
     }
  
